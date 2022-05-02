8e8796ad7dba1c300785f23ec60e82af813cd706
sardoraminov <oddiydasturchi@gmail.com>
2022-05-02 23:04:51 +0500

    make it better

--- utils/upload.js
+++ utils/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../dist'));
+    cb(null, path.join(__dirname, "../dist"));
   },
   filename: function (req, file, cb) {
     cb(null, `${file.originalname} - ${Date.now()}`);
